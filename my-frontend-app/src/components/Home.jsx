4098d4949e2a5675af87d99cafa5156ab16ed26e
Andrey-Mikhailov-Fro <dark.ace.frost@gmail.com>
2024-07-21 17:13:33 +0300

    fix socket connection #1

--- my-frontend-app/src/components/Home.jsx
+++ my-frontend-app/src/components/Home.jsx
@@ -9,7 +9,7 @@ import ChannelsBlock from './ChannelsBlock';
 import MessagesBlock from './MessagesBlock';
 import CommonHeader from './CommonHeader';
 
-const socket = io('http://localhost:5001');
+const socket = io('https://localhost:5001');
 
 function Home() {
   const [activeChannel, setActive] = useState('1');
